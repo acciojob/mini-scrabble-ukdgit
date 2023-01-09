9a1cd9bc456347bc04789291a85ec83f0a3e8774
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-01-09 13:30:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your code here
+let count = 0;
+let text = "";
+document.getElementById("evaluatedText").addEventListener("keyup", function () {
+  text = document.getElementById("evaluatedText").value;
+  console.log(text);
+  count = text.length;
+  document.getElementById("letterCount").innerText = count;
+});
